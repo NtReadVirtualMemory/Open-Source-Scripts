c4f2a63d32c2ce245d7a489355ff8115e47afb9b
NtReadVirtualMemory <83749737+NtReadVirtualMemory@users.noreply.github.com>
2024-10-05 17:57:19 +0200

    Update and rename Auto Rob to Auto Rob.lua

--- Mad City Chapter 1/Auto Rob.lua
+++ Mad City Chapter 1/Auto Rob.lua
@@ -10,6 +10,10 @@ if _G.AutoRob == true then warn("Auto Rob already loaded.") return nil end
 _G.AutoRob = true
 queue_on_teleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/NtReadVirtualMemory/Open-Source-Scripts/refs/heads/main/Mad%20City%20Chapter%201/Auto%20Rob'))()")
 
+for i = 1,100 do
+   print("Made by NtOpenProcess and deni210 (on dc)")
+end
+
 function shop()
     local a,b = pcall(function()
 
